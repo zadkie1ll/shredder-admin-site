45a135478d76f83a3256f7794ca86d352c1e4bd1
Andrey <apugachevdev@gmail.com>
2026-04-21 01:41:14 +0300

    upd

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,4 +4,8 @@ set -e
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
-exec gunicorn --bind 0.0.0.0:8000 web_app.wsgi:application
+exec gunicorn \
+  --bind 0.0.0.0:8000 \
+  --workers "${GUNICORN_WORKERS:-3}" \
+  --timeout "${GUNICORN_TIMEOUT:-60}" \
+  web_app.wsgi:application
--- web_app/settings.py
+++ web_app/settings.py
@@ -151,15 +151,8 @@ WSGI_APPLICATION = "web_app.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
-DATABASES = {
     'default': config(
-        'DATABASE_URL',
+        'WEB_DATABASE_URL',
         default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
         cast=dj_database_url.parse
     )
